2f594662ee5683d25e10b0aa0e34478da5fd8584
Voievodina <vjn21@ukr.net>
2021-01-29 13:26:40 +0200

    version2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 
 @Component({
@@ -7,7 +8,33 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.css'],
   host: {
     '(document:click)': 'clearSearch($event)',
-  }
+  },
+  animations: [
+    trigger(
+      'openAnimation', [
+        transition(':enter', [
+          style({ 
+          left:"93%",
+           width:"0%",
+          }), 
+          animate('0.5s ease', style({ 
+            left:"0",
+            width:'90%',
+          }))
+        ]),
+        transition(':leave', [          
+          style({width:"90%", 
+          left:"0",
+            }),    
+          animate('0.5s ease',
+           style({
+             width:"0%",
+              left:"93%"
+            }))
+      ])
+      ]
+    )
+  ],
 })
 
 export class AppComponent {
